73393bda746c6932f472a73785655996e54747bc
Selin Şeker <116524939+selinseker@users.noreply.github.com>
2024-07-26 14:27:21 +0300

    Update ViewController.swift
    adding countdown features

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,32 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var SecondRemaining = 60
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle
+        
+        SecondRemaining = eggTimes[hardness!]!
+        
+       timer =  Timer.scheduledTimer(timeInterval: 1.0 , target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        
+        if SecondRemaining > 0{
+        
+           print("\(SecondRemaining) second.")
+           SecondRemaining -= 1
+        }
+    }
 
 }
